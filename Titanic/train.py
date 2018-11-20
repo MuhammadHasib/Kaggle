c323169b08f0d1a7422bcae7ec99ab2570b09275
Muhammad Hasib <hasibdelhi@gmail.com>
2018-11-20 10:31:06 +0100

    Change in train.py

--- Titanic/train.py
+++ Titanic/train.py
@@ -1,3 +1,7 @@
+import numpy as np
 import pandas as pd
-data = pd.read_csv("gender_submission.csv")
-data.head() 
+from collections import Counter
+
+df_train = pd.read_csv('train.csv')
+df_test  = pd.read_csv('test.csv')
+df_sample= pd.read_csv('gender_submission.csv')
