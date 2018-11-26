839d719e46fca4882664da4a6aa32c4d1a7fef7a
Muhammad Hasib <hasibdelhi@gmail.com>
2018-11-26 12:19:54 +0100

    Change Santa.py

--- Traveling_Santa/Santa.py
+++ Traveling_Santa/Santa.py
@@ -11,3 +11,9 @@ sns.set_style('whitegrid')
 
 df = pd.read_csv("cities.csv")
 df.head()
+
+plt.figure(figsize=(15, 10))
+plt.scatter(df.X, df.Y, s=1)
+plt.scatter(df.iloc[0: 1, 1], df.iloc[0: 1, 2], s=10, c="red")
+plt.grid(False)
+plt.show()
