f205c6134cf1b34c0a575bc99c589df7e10c656b
Muhammad Hasib <hasibdelhi@gmail.com>
2018-11-26 12:21:20 +0100

    Change Santa.py

--- Traveling_Santa/Santa.py
+++ Traveling_Santa/Santa.py
@@ -20,3 +20,13 @@ plt.show()
 
 nb_cities = max(df.CityId)
 print("Number of cities to visit : ", nb_cities)
+
+def sieve_eratosthenes(n):
+    primes = [False, False] + [True for i in range(n-1)]
+    p = 2
+    while (p * p <= n):
+        if (primes[p] == True):
+            for i in range(p * 2, n + 1, p):
+                primes[i] = False
+        p += 1
+    return primes
