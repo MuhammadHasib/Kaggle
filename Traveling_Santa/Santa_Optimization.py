41e00eb13222b09de0052a5407f5e499eed3138c
Muhammad Hasib <hasibdelhi@gmail.com>
2018-12-11 12:00:24 +0100

    Santa_Optimization.py

---
+++ Traveling_Santa/Santa_Optimization.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import NearestNeighbors
+import os
+print(os.listdir("../input"))
+
+pd.options.mode.chained_assignment = None  # default='warn'
+
+# calculate total distance of the path
+def total_distance(dfcity,path):
+    prev_city = path[0]
+    total_distance = 0
+    step_num = 1
+    for city_num in path[1:]:
+        next_city = city_num
+        total_distance = total_distance + \
+            np.sqrt(pow((dfcity.X[city_num] - dfcity.X[prev_city]),2) + pow((dfcity.Y[city_num] - dfcity.Y[prev_city]),2)) * \
+            (1+ 0.1*((step_num % 10 == 0)*int(not(prime_cities[prev_city]))))
+        prev_city = next_city
+        step_num = step_num + 1
+    return total_distance
